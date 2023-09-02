a8415614a5d1530c56b4736c10a41ba34a201189
Bill Wang <bill.wang>
2023-09-03 00:35:16 +1000

    upgrade to azurerm 3.0.1+

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "hashicorp/azurerm"
       # Root module should specify the maximum provider version
       # The ~> operator is a convenient shorthand for allowing only patch releases within a specific minor release.
-      version = "~> 2.26"
+      version = ">=3.0.1"
     }
   }
 }
@@ -33,35 +33,27 @@ resource "azurerm_application_insights" "application_insights" {
   application_type    = "Node.JS"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "${var.project}-${var.environment}-app-service-plan"
-  resource_group_name = azurerm_resource_group.resource_group.name
   location            = var.location
-  kind                = "FunctionApp"
-  reserved = true
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  resource_group_name = azurerm_resource_group.resource_group.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_function_app" "function_app" {
-  name                       = "${var.project}-${var.environment}-function-app"
-  resource_group_name        = azurerm_resource_group.resource_group.name
-  location                   = var.location
-  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE"       = "",
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
-  }
-  os_type = "linux"
+resource "azurerm_linux_function_app" "function_app" {
+  name                = "${var.project}-${var.environment}-function-app"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  version                    = "~3"
 
-  lifecycle {
-    ignore_changes = [
-      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
-    ]
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
   }
 }
+
